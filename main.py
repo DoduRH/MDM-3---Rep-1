0520c39371e926fa4bc001d03de44ed54371997b
Ben <ei18017@bristol.ac.uk>
2020-10-27 13:20:32 +0000

    Implemented the poisson distribution for the arrival of vehicles Made vehicles awareness of other vehicles and if they are to close to another vehicle they stop

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pygame as pg
 import vehicle
 import obstacle
 import road
-import random
 import time
 import numpy as np
 import pandas as pd
@@ -20,30 +19,47 @@ clock = pg.time.Clock()
 simQuit = False
 
 # Create Objects for simulation
-car = vehicle.Vehicle([0, (gV.displaySize[1]/2)-10], (40, 20), [0, 0], [3, 0])
-obstruction = obstacle.Obstacle([gV.displaySize[0]/1.5, (gV.displaySize[1]/2)], (30, 40))
-road = road.Road([0, (gV.displaySize[1]/2)-gV.roadWidth/2], 100, 1, gV.roadWidth)
+vehicleArray = []
+obstructionArray = [obstacle.Obstacle([gV.displaySize[0]/1.5, (gV.displaySize[1]/2)], (30, 40))]
+roadObject = road.Road([0, (gV.displaySize[1]/2)-gV.roadWidth/2], 100, 1, gV.roadWidth, 0.06166666666666667)
+
+
+def generateTraffic(road):
+    for arrivals in range(0, np.random.poisson(road.meanArrivalRate)):
+        vehicleArray.append(vehicle.Vehicle([-100, (gV.displaySize[1] / 2) - 10], (40, 20), [0, 0], [3, 0]))
+
 
 # Main loop
 while not simQuit:
     gV.deltaTime = clock.tick(gV.fps) / 1000
+    gV.runTimer += gV.deltaTime
+    simDisplay.fill(gV.white)
+    roadObject.draw(simDisplay)
 
-    # Main event loop
+    # Event handling loop
     for event in pg.event.get():
-        # If red cross pressed then quit window
+        # If red cross pressed then quit main loop
         if event.type == pg.QUIT:
             simQuit = True
 
-    # vehicle handling loop
-    car.checkHazards(road, obstruction)
-    car.move(road)
+    # generate traffic coming down road frequency dependent on poisson distribution
+    if round(gV.runTimer, 1) % 1 == 0:
+        generateTraffic(roadObject)
 
-    simDisplay.fill(gV.white)
-    road.draw(simDisplay)
-    car.draw(simDisplay)
-    obstruction.draw(simDisplay)
+    # vehicle handling loop
+    for vehicleObject in vehicleArray:
+        vehicleObject.checkHazards(roadObject, vehicleArray, obstructionArray)
+        vehicleObject.move(roadObject)
+        vehicleObject.draw(simDisplay)
+
+    # obstruction handling loop
+    for obstacleObject in obstructionArray:
+        obstacleObject.draw(simDisplay)
 
     pg.display.update()
 
 pg.quit()
 quit()
+
+
+
--- road.py
+++ road.py
@@ -5,11 +5,12 @@ import globalVariables as gV
 
 class Road:
 
-    def __init__(self, pos, speedLimit, laneCount, laneWidth):
+    def __init__(self, pos, speedLimit, laneCount, laneWidth, meanArrivalRate):
         self.pos = pos
         self.speedLimit = speedLimit
         self.laneCount = laneCount
         self.laneWidth = laneWidth
+        self.meanArrivalRate = meanArrivalRate
 
     def draw(self, display):
         pg.draw.rect(display, gV.grey, [self.pos[0], self.pos[1], gV.displaySize[0], self.laneWidth])
